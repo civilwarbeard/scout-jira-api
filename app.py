1c6edc9d77e964fa6d7dc6a2908544528aa064ec
Nick Janquart <nick.janquart@wellspring.com>
2023-11-13 17:08:50 -0600

    updating templates and using os var

--- app.py
+++ app.py
@@ -12,7 +12,29 @@ app = Flask(__name__)
 
 @app.route("/")
 def landing_page():
-    return render_template("landing.html")
+    headers = {
+  'Authorization': BEARER_TOKEN
+  }
+
+    #URLS
+    #setup API route/keys
+    base_url = "https://jira.wellspring.com/rest/"
+
+    #Scout (Starfox) Board = 94
+    active_sprint = "agile/1.0/board/94/sprint?state=active"
+    all_issues = "agile/1.0/board/94/sprint/"
+    issues = "/issue"
+
+    #Get sprint current sprint id for issues request
+    response = requests.request("GET", base_url+active_sprint, headers=headers)
+    current_sprint = response.json()
+    sprint_id = current_sprint["values"][0]["id"]
+
+    response = requests.request("GET", base_url+all_issues+str(sprint_id)+issues, headers=headers)
+    current_sprint_issues = response.json()
+    return render_template("landing.html",
+        current_sprint_issues=current_sprint_issues,
+        sprint_id=sprint_id)
 
 @app.route("/help", methods=["GET"])
 def help():
@@ -21,48 +43,50 @@ def help():
 @app.route("/current", methods=["GET"])
 def current():
     headers = {
-  'Authorization': 'Bearer OTczOTU4Mjg2ODMxOl0tPXcAsFVOeiUT6NUs8muGJk13'
+  'Authorization': 'Bearer OTQ1MjQ3ODMwNjMxOvSONf0GauaTrb/N3FZo1KB5rM7u'
   }
 
+    #URLS
     #setup API route/keys
     base_url = "https://jira.wellspring.com/rest/"
 
     #Scout (Starfox) Board = 94
     active_sprint = "agile/1.0/board/94/sprint?state=active"
-
-    #Pulls back issues for current sprint
     all_issues = "agile/1.0/board/94/sprint/"
     issues = "/issue"
+
+    #Get sprint current sprint id for issues request
     response = requests.request("GET", base_url+active_sprint, headers=headers)
+    current_sprint = response.json()
+    sprint_id = current_sprint["values"][0]["id"]
 
-    #Get raw data
-    active_scout_sprint = response.json()
-
-    #get sprint id from json response
-    sprint_id = active_scout_sprint["values"][0]["id"]
-    
-    #Get raw data
     response = requests.request("GET", base_url+all_issues+str(sprint_id)+issues, headers=headers)
-    active_scout_issues = response.json()
-    list_of_issues = active_scout_issues["issues"]
+    current_sprint_issues = response.json()
 
     ##Sprint Totals
     points = []
-    for item in active_scout_issues["issues"]:
+    for item in current_sprint_issues["issues"]:
         points.append(item["fields"]["customfield_10106"])
 
     total_points = sum(points)
 
-    #Filters for Columns
-    maintenance_issues = filter(lambda x: "Maintenance" in x["fields"]["labels"], active_scout_issues["issues"])
-    mr_index_issues = filter(lambda x: "MrIndex" in x["fields"]["labels"], active_scout_issues["issues"])
-    client_issues = filter(lambda x: "Client" in x["fields"]["labels"], active_scout_issues["issues"])
-    project_issues = filter(lambda x: "project" in x["fields"]["labels"], active_scout_issues["issues"])
+    #Filters for Columns   NEED TO UPDATE Filters to be accepted / not accepted AND label specific... shoudl 
+    #have 6 total and maybe overall accepted
+    maintenance_issues = filter(lambda x: "Maintenance" in x["fields"]["labels"], current_sprint_issues["issues"])
+    mr_index_issues = filter(lambda x: "MrIndex" in x["fields"]["labels"], current_sprint_issues["issues"])
+    client_issues = filter(lambda x: "Client" in x["fields"]["labels"], current_sprint_issues["issues"])
+    project_issues = filter(lambda x: "project" in x["fields"]["labels"], current_sprint_issues["issues"])
+    accepted_issues = filter(lambda x: "Accepted" in x["fields"]["status"]["name"], current_sprint_issues["issues"])
+    accepted_client_issues = filter(lambda x: "Accepted" in x["fields"]["status"]["name"] and "Client" in x["fields"]["labels"], 
+        current_sprint_issues["issues"])
     
     return render_template("current.html",
-        list_of_issues = list_of_issues,
+        current_sprint_issues = current_sprint_issues,
         maintenance_issues = maintenance_issues,
         mr_index_issues = mr_index_issues,
         client_issues = client_issues,
         project_issues = project_issues,
-        total_points = total_points)
+        total_points = total_points,
+        accepted_issues = accepted_issues,
+        accepted_client_issues = accepted_client_issues,
+        sprint_id = sprint_id)
